648168cbcfc94f444e135f86ca35fd7059b88991
mnjoki21 <mitchellenjoki7@gmail.com>
2022-08-29 15:22:01 +0300

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,10 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+  
+end
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/artists.sqlite"
+  )
+  
